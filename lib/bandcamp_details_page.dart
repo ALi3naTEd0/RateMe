0a90ef020f8ffb939ec3c8be3280cf52dea51da4
ALi3naTEd0 <ALi3naTEd0@protonmail.com>
2024-06-08 00:27:43 -0600

    Rate Me! v0.0.9-3

--- lib/bandcamp_details_page.dart
+++ lib/bandcamp_details_page.dart
@@ -1,3 +1,4 @@
+// bandcamp_details_page.dart
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher.dart';
@@ -6,7 +7,7 @@ import 'bandcamp_parser.dart';
 import 'footer.dart';
 import 'app_theme.dart';
 import 'user_data.dart';
-import 'id_generator.dart'; // Import the UniqueIdGenerator
+import 'id_generator.dart';
 
 class BandcampDetailsPage extends StatefulWidget {
   final dynamic album;
--- lib/id_generator.dart
+++ lib/id_generator.dart
@@ -1,8 +1,14 @@
 class UniqueIdGenerator {
   static int _lastCollectionId = 1000000000;
+  static int _lastTrackId = 2000000000;
 
   static int generateUniqueCollectionId() {
     _lastCollectionId++;
     return _lastCollectionId;
+  }
+
+  static int generateUniqueTrackId() {
+    _lastTrackId++;
+    return _lastTrackId;
   }
 }
