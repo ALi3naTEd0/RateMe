e23c82c4c83a0efe06c3c854f9ca881b5b7740db
ALi3naTEd0 <ALi3naTEd0@protonmail.com>
2024-06-07 21:18:03 -0600

    Rate Me! v0.0.9-3

--- lib/bandcamp_service.dart
+++ lib/bandcamp_service.dart
@@ -1,5 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as html_parser;
+import 'id_generator.dart'; // Import the UniqueIdGenerator
+import 'bandcamp_parser.dart'; // Import the BandcampParser
 
 class BandcampService {
   static Future<Map<String, dynamic>> fetchBandcampAlbumInfo(String url) async {
@@ -11,11 +13,16 @@ class BandcampService {
       String artist = document.querySelector('meta[property="og:site_name"]')?.attributes['content'] ?? 'Unknown Artist';
       String artworkUrl = document.querySelector('meta[property="og:image"]')?.attributes['content'] ?? '';
 
+      // Generar un ID único para el álbum
+      int collectionId = UniqueIdGenerator.generateUniqueCollectionId();
+
       return {
+        'collectionId': collectionId, // Assign unique collectionId
         'collectionName': title,
         'artistName': artist,
         'artworkUrl100': artworkUrl,
         'url': url,
+        'tracks': BandcampParser.extractTracks(document, collectionId), // Extract tracks with collectionId
       };
     } else {
       throw Exception('Failed to load Bandcamp album');
