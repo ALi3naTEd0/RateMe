e0d189445be8a01146235e043784d8ce06de0c4a
ALi3naTEd0 <ALi3naTEd0@protonmail.com>
2024-08-07 21:28:51 -0600

    added logging.dart

---
+++ lib/logging.dart
@@ -0,0 +1,33 @@
+import 'package:logging/logging.dart';
+
+class Logging {
+  static final Logger _logger = Logger('AppLogger');
+
+  // Configura el logger
+  static void setupLogging() {
+    Logger.root.level = Level.ALL; // Establece el nivel de logging
+    Logger.root.onRecord.listen((record) {
+      final logMessage =
+          '${record.level.name}: ${record.time}: ${record.message}';
+      if (record.error != null) {
+        print(
+            '$logMessage, Error: ${record.error}, StackTrace: ${record.stackTrace}');
+      } else {
+        print(logMessage);
+      }
+    });
+  }
+
+  // Métodos de acceso para diferentes niveles de logging
+  static void info(String message, [Object? error, StackTrace? stackTrace]) {
+    _logger.info(message, error, stackTrace);
+  }
+
+  static void warning(String message, [Object? error, StackTrace? stackTrace]) {
+    _logger.warning(message, error, stackTrace);
+  }
+
+  static void severe(String message, [Object? error, StackTrace? stackTrace]) {
+    _logger.severe(message, error, stackTrace);
+  }
+}
