ef89cf2c0d3e14e142fdb08fb9ccc738d15dd4df
ALi3naTEd0 <ALi3naTEd0@protonmail.com>
2025-02-19 15:14:08 -0600

    fix: update Windows window title to rateme

--- windows/runner/win32_window.cpp
+++ windows/runner/win32_window.cpp
@@ -120,24 +120,25 @@ Win32Window::~Win32Window() {
   Destroy();
 }
 
-bool Win32Window::Create(const wchar_t* title, const Point& origin,
-                        const Size& size) {
+bool Win32Window::Create(const std::wstring& title,
+                         const Point& origin,
+                         const Size& size) {
   Destroy();
 
-  WNDCLASS window_class = RegisterWindowClass();
+  const wchar_t* window_class =
+      WindowClassRegistrar::GetInstance()->GetWindowClass();
 
   const POINT target_point = {static_cast<LONG>(origin.x),
-                            static_cast<LONG>(origin.y)};
+                              static_cast<LONG>(origin.y)};
   HMONITOR monitor = MonitorFromPoint(target_point, MONITOR_DEFAULTTONEAREST);
   UINT dpi = FlutterDesktopGetDpiForMonitor(monitor);
   double scale_factor = dpi / 96.0;
 
   HWND window = CreateWindow(
-      window_class.lpszClassName, title,
-      WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+      window_class, title.c_str(), WS_OVERLAPPEDWINDOW,
       Scale(origin.x, scale_factor), Scale(origin.y, scale_factor),
       Scale(size.width, scale_factor), Scale(size.height, scale_factor),
-      nullptr, nullptr, window_class.hInstance, this);
+      nullptr, nullptr, GetModuleHandle(nullptr), this);
 
   if (!window) {
     return false;
