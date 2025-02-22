c4b59045ac6e3e04e2e387f0547c66306a57df42
ALi3naTEd0 <ALi3naTEd0@protonmail.com>
2025-02-21 20:41:27 -0600

    feat: MacOS workflow DMG (experimental)

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -5,15 +5,11 @@ class MainFlutterWindow: NSWindow {
     override func awakeFromNib() {
         let flutterViewController = FlutterViewController()
         let windowFrame = self.frame
-        
-        // Ensure view initialization
-        if let view = flutterViewController.view {
-            view.frame = NSRect(x: 0, y: 0, width: windowFrame.width, height: windowFrame.height)
-        }
-        
         self.contentViewController = flutterViewController
         self.setFrame(windowFrame, display: true)
-        self.backgroundColor = NSColor.white
+        
+        // Add this line to fix black screen
+        flutterViewController.view.wantsLayer = true
 
         RegisterGeneratedPlugins(registry: flutterViewController)
 
