e307e33288a8bc1a5cc0bfb040170391e9fa132f
ALi3naTEd0 <ALi3naTEd0@protonmail.com>
2025-02-21 20:28:03 -0600

    feat: MacOS workflow DMG (experimental)

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -2,14 +2,21 @@ import Cocoa
 import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
-  override func awakeFromNib() {
-    let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
-    self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    override func awakeFromNib() {
+        let flutterViewController = FlutterViewController()
+        let windowFrame = self.frame
+        
+        // Ensure view initialization
+        if let view = flutterViewController.view {
+            view.frame = NSRect(x: 0, y: 0, width: windowFrame.width, height: windowFrame.height)
+        }
+        
+        self.contentViewController = flutterViewController
+        self.setFrame(windowFrame, display: true)
+        self.backgroundColor = NSColor.white
 
-    RegisterGeneratedPlugins(registry: flutterViewController)
+        RegisterGeneratedPlugins(registry: flutterViewController)
 
-    super.awakeFromNib()
-  }
+        super.awakeFromNib()
+    }
 }
