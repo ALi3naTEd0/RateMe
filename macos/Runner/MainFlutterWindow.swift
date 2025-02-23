86b2484969c6098cd5afbef0cc9c3215523e9bd5
ALi3naTEd0 <ALi3naTEd0@protonmail.com>
2025-02-22 20:59:51 -0600

    feat: macos DMG testing (experimental)

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,22 +4,12 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
     override func awakeFromNib() {
         let flutterViewController = FlutterViewController()
-        
-        // Initialize view and frame
-        if let flutterView = flutterViewController.view {
-            flutterView.wantsLayer = true
-            flutterView.layer?.backgroundColor = NSColor.white.cgColor
-        }
-        
         let windowFrame = self.frame
         self.contentViewController = flutterViewController
         self.setFrame(windowFrame, display: true)
-        
-        // Basic window configuration
-        self.titlebarAppearsTransparent = false
-        self.backgroundColor = NSColor.white
 
         RegisterGeneratedPlugins(registry: flutterViewController)
+
         super.awakeFromNib()
     }
 }
