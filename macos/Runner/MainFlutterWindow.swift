ca6cfe862c6c0fb81ba06f68d52e4a20fe323953
ALi3naTEd0 <ALi3naTEd0@protonmail.com>
2025-02-22 21:37:46 -0600

    feat: macos DMG testing

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -5,11 +5,19 @@ class MainFlutterWindow: NSWindow {
     override func awakeFromNib() {
         let flutterViewController = FlutterViewController()
         let windowFrame = self.frame
+        
+        // Set window properties
+        self.contentMinSize = NSSize(width: 800, height: 600)
+        self.contentAspectRatio = NSSize(width: 16, height: 9)
+        self.titlebarAppearsTransparent = true
+        
+        // Set content view controller
         self.contentViewController = flutterViewController
         self.setFrame(windowFrame, display: true)
-
+        
+        // Register plugins
         RegisterGeneratedPlugins(registry: flutterViewController)
-
+        
         super.awakeFromNib()
     }
 }
