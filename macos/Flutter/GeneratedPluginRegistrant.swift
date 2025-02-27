58304b4a37b6411a7500fc97d7104b1c65ca51c8
ALi3naTEd0 <ALi3naTEd0@protonmail.com>
2025-02-27 01:48:54 -0600

    chore: restore platform files and fix url_launcher
    - Restore CMake and generated files for Linux/Windows
    - Restore Flutter window files for Windows
    - Restore GeneratedPluginRegistrant for macOS
    - Add browser queries for url_launcher in AndroidManifest.xml

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import file_picker
+import package_info_plus
+import path_provider_foundation
+import share_plus
+import shared_preferences_foundation
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
